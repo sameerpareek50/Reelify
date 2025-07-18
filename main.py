9ba839acbcbd7acfc9886e86db1ddff44ca6f9ac
Sameer pareek <sameerpareek500@gmail.com>
2025-07-18 13:22:20 +0530

    used flask for uploading files and provided key value pairs to each upload and it will be unique due to uuid

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template , request
 import uuid # provide uniques id to all different folders
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'user_uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def home():
@@ -14,10 +20,15 @@ def create():
 
     if request.method == "POST":
         print(request.files.keys())
+        rec_id = request.form.get("uuid")   # received unique id
+        desc = request.form.get("text")      # Description
         for key,value in request.files.items():
             print(f"Key: {key}, Value: {value}")
+            # upload the file
+            # key is file1, file2 etc   and value is the file name like image name
 
     return render_template("create.html", myid=myid)
+
 
 @app.route("/gallery")
 def gallery():
