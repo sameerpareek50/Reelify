a0d280e540e788c5073594e8a3210d87ca6a109c
Sameer pareek <sameerpareek500@gmail.com>
2025-07-18 12:28:43 +0530

    Added uuid option for getting different names

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
+import uuid # provide uniques id to all different folders
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html") 
 
-@app.route("/create")
+@app.route("/create", methods=["GET", "POST"])
 def create():
-    return render_template("create.html")
+    myid = uuid.uuid1()
+    print(f"Unique ID for this session: {myid}")
+
+    if request.method == "POST":
+        print(request.files.keys())
+        for key,value in request.files.items():
+            print(f"Key: {key}, Value: {value}")
+
+    return render_template("create.html", myid=myid)
 
 @app.route("/gallery")
 def gallery():
@@ -16,3 +25,4 @@ def gallery():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
